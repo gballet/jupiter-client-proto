3e755704a1406d949f2ee46106f2da0dc89ef5c0
Guillaume Ballet <gballet@gmail.com>
2020-07-09 20:54:28 +0200

    move db management functions to their own file

---
+++ src/db.rs
@@ -0,0 +1,146 @@
+use super::JupiterError;
+use jupiter_account::Account;
+use multiproof_rs::{ByteKey, NibbleKey, Node, Tree};
+use rusqlite::{Connection, Row, NO_PARAMS};
+
+pub fn initdb(dbfilename: &str) -> Result<Connection, JupiterError> {
+    let conn = Connection::open(dbfilename)?;
+
+    conn.execute(
+        "create table if not exists leaves(id integer primary key, key blob, value blob);",
+        NO_PARAMS,
+    )?;
+
+    conn.execute("CREATE TABLE IF NOT EXISTS root(hash blob);", NO_PARAMS)?;
+
+    let rootcount: u32 = conn.query_row("select count(*) FROM root;", NO_PARAMS, |row| {
+        Ok(row.get(0)?)
+    })?;
+    if rootcount == 0 {
+        let empty_root = Node::EmptySlot;
+        conn.execute(
+            format!(
+                "INSERT INTO root (hash) values (X'{}')",
+                hex::encode(empty_root.hash())
+            )
+            .as_str(),
+            NO_PARAMS,
+        )?;
+    }
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS logs(id integer primary key, type string, sender blob, recipient blob, data blob);",
+        NO_PARAMS,
+    )?;
+
+    Ok(conn)
+}
+
+pub fn get_root(db: &Connection) -> Vec<u8> {
+    let h: Vec<u8> = db
+        .query_row("select hash FROM root;", NO_PARAMS, |row| {
+            Ok(row.get::<_, Vec<u8>>(0)?)
+        })
+        .unwrap();
+    h
+}
+
+pub fn update_root(db: &Connection, hash: Vec<u8>) -> rusqlite::Result<()> {
+    println!("Setting trie root to {:?}", hex::encode(hash.clone()));
+    db.execute(
+        format!("UPDATE root SET hash = X'{}'", hex::encode(hash)).as_str(),
+        NO_PARAMS,
+    )?;
+    Ok(())
+}
+
+pub fn has_root(db: &Connection) -> bool {
+    let count: u32 = db
+        .query_row("select count(*) FROM root;", NO_PARAMS, |row| {
+            Ok(row.get(0)?)
+        })
+        .unwrap();
+    count > 0
+}
+
+pub fn update_leaf(db: &Connection, key: NibbleKey, value: Vec<u8>) -> rusqlite::Result<usize> {
+    db.execute(
+        format!(
+            "UPDATE leaves SET value = X'{}' WHERE key = X'{}';",
+            hex::encode(value),
+            hex::encode::<Vec<u8>>(ByteKey::from(key).into()),
+        )
+        .as_str(),
+        NO_PARAMS,
+    )
+}
+
+pub fn insert_leaf(db: &Connection, key: NibbleKey, value: Vec<u8>) -> rusqlite::Result<usize> {
+    db.execute(
+        format!(
+            "INSERT INTO leaves (key, value) VALUES (X'{}', X'{}');",
+            hex::encode::<Vec<u8>>(ByteKey::from(key).into()),
+            hex::encode(value),
+        )
+        .as_str(),
+        NO_PARAMS,
+    )
+}
+
+pub fn log_tx(
+    db: &Connection,
+    operation: &str,
+    from: NibbleKey,
+    to: NibbleKey,
+    val: Vec<u8>,
+) -> rusqlite::Result<usize> {
+    db.execute(
+        format!(
+            "INSERT INTO logs (type, sender, recipient, data) VALUES ('{}', '{}', '{}', X'{}');",
+            operation,
+            hex::encode::<Vec<u8>>(ByteKey::from(from).into()),
+            hex::encode::<Vec<u8>>(ByteKey::from(to).into()),
+            hex::encode(val)
+        )
+        .as_str(),
+        NO_PARAMS,
+    )
+}
+
+pub fn extract_key(row: &Row) -> rusqlite::Result<(NibbleKey, Vec<u8>)> {
+    let bkey = row.get::<_, Vec<u8>>(0)?;
+    let k: NibbleKey = NibbleKey::from(ByteKey::from(bkey));
+    let v: Vec<u8> = row.get(1)?;
+    Ok((k, v))
+}
+
+pub fn rebuild_trie(db: &Connection) -> rusqlite::Result<Node> {
+    let mut root = Node::EmptySlot;
+    if has_root(db) {
+        let mut stmt = db.prepare("SELECT key, value FROM leaves ORDER BY key;")?;
+        let keyvals = stmt.query_map(NO_PARAMS, extract_key)?;
+
+        for kvr in keyvals {
+            let (key, val) = kvr?;
+            root.insert(&key, val.to_vec()).unwrap();
+        }
+    }
+
+    Ok(root)
+}
+
+pub fn get_account(db: &Connection, addr: &str) -> rusqlite::Result<Account> {
+    if !has_root(db) {
+        panic!("db has no root");
+    }
+
+    let val = db
+        .query_row(
+            format!("SELECT value FROM leaves WHERE key = X'{}'", addr).as_str(),
+            NO_PARAMS,
+            |row| row.get::<_, Vec<u8>>(0),
+        )
+        .unwrap_or_default();
+
+    Ok(rlp::decode(&val).unwrap())
+}
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ extern crate rusqlite;
 extern crate secp256k1;
 extern crate sha3;
 
+mod db;
+
 use clap::{App, Arg, SubCommand};
+use db::*;
 use jupiter_account::{Account, Tx, TxData};
 use multiproof_rs::{make_multiproof, ByteKey, NibbleKey, Node, ProofToTree, Tree};
 use rand::{thread_rng, Rng};
-use rusqlite::{Connection, Row, NO_PARAMS};
+use rusqlite::NO_PARAMS;
 use secp256k1::{recover as secp256k1_recover, sign as secp256k1_sign, Message, SecretKey};
 use sha3::{Digest, Keccak256};
 
@@ -34,148 +37,6 @@ impl From<&str> for JupiterError {
     fn from(s: &str) -> Self {
         JupiterError(format!("{}", s))
     }
-}
-
-fn initdb(dbfilename: &str) -> Result<Connection, JupiterError> {
-    let conn = Connection::open(dbfilename)?;
-
-    conn.execute(
-        "create table if not exists leaves(id integer primary key, key blob, value blob);",
-        NO_PARAMS,
-    )?;
-
-    conn.execute("CREATE TABLE IF NOT EXISTS root(hash blob);", NO_PARAMS)?;
-
-    let rootcount: u32 = conn.query_row("select count(*) FROM root;", NO_PARAMS, |row| {
-        Ok(row.get(0)?)
-    })?;
-    if rootcount == 0 {
-        let empty_root = Node::EmptySlot;
-        conn.execute(
-            format!(
-                "INSERT INTO root (hash) values (X'{}')",
-                hex::encode(empty_root.hash())
-            )
-            .as_str(),
-            NO_PARAMS,
-        )?;
-    }
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS logs(id integer primary key, type string, sender blob, recipient blob, data blob);",
-        NO_PARAMS,
-    )?;
-
-    Ok(conn)
-}
-
-fn get_root(db: &Connection) -> Vec<u8> {
-    let h: Vec<u8> = db
-        .query_row("select hash FROM root;", NO_PARAMS, |row| {
-            Ok(row.get::<_, Vec<u8>>(0)?)
-        })
-        .unwrap();
-    h
-}
-
-fn update_root(db: &Connection, hash: Vec<u8>) -> rusqlite::Result<()> {
-    println!("Setting trie root to {:?}", hex::encode(hash.clone()));
-    db.execute(
-        format!("UPDATE root SET hash = X'{}'", hex::encode(hash)).as_str(),
-        NO_PARAMS,
-    )?;
-    Ok(())
-}
-
-fn has_root(db: &Connection) -> bool {
-    let count: u32 = db
-        .query_row("select count(*) FROM root;", NO_PARAMS, |row| {
-            Ok(row.get(0)?)
-        })
-        .unwrap();
-    count > 0
-}
-
-fn update_leaf(db: &Connection, key: NibbleKey, value: Vec<u8>) -> rusqlite::Result<usize> {
-    db.execute(
-        format!(
-            "UPDATE leaves SET value = X'{}' WHERE key = X'{}';",
-            hex::encode(value),
-            hex::encode::<Vec<u8>>(ByteKey::from(key).into()),
-        )
-        .as_str(),
-        NO_PARAMS,
-    )
-}
-
-fn insert_leaf(db: &Connection, key: NibbleKey, value: Vec<u8>) -> rusqlite::Result<usize> {
-    db.execute(
-        format!(
-            "INSERT INTO leaves (key, value) VALUES (X'{}', X'{}');",
-            hex::encode::<Vec<u8>>(ByteKey::from(key).into()),
-            hex::encode(value),
-        )
-        .as_str(),
-        NO_PARAMS,
-    )
-}
-
-fn log_tx(
-    db: &Connection,
-    operation: &str,
-    from: NibbleKey,
-    to: NibbleKey,
-    val: Vec<u8>,
-) -> rusqlite::Result<usize> {
-    db.execute(
-        format!(
-            "INSERT INTO logs (type, sender, recipient, data) VALUES ('{}', '{}', '{}', X'{}');",
-            operation,
-            hex::encode::<Vec<u8>>(ByteKey::from(from).into()),
-            hex::encode::<Vec<u8>>(ByteKey::from(to).into()),
-            hex::encode(val)
-        )
-        .as_str(),
-        NO_PARAMS,
-    )
-}
-
-fn extract_key(row: &Row) -> rusqlite::Result<(NibbleKey, Vec<u8>)> {
-    let bkey = row.get::<_, Vec<u8>>(0)?;
-    let k: NibbleKey = NibbleKey::from(ByteKey::from(bkey));
-    let v: Vec<u8> = row.get(1)?;
-    Ok((k, v))
-}
-
-fn rebuild_trie(db: &Connection) -> rusqlite::Result<Node> {
-    let mut root = Node::EmptySlot;
-    if has_root(db) {
-        let mut stmt = db.prepare("SELECT key, value FROM leaves ORDER BY key;")?;
-        let keyvals = stmt.query_map(NO_PARAMS, extract_key)?;
-
-        for kvr in keyvals {
-            let (key, val) = kvr?;
-            root.insert(&key, val.to_vec()).unwrap();
-        }
-    }
-
-    Ok(root)
-}
-
-fn get_account(db: &Connection, addr: &str) -> rusqlite::Result<Account> {
-    if !has_root(db) {
-        panic!("db has no root");
-    }
-
-    let val = db
-        .query_row(
-            format!("SELECT value FROM leaves WHERE key = X'{}'", addr).as_str(),
-            NO_PARAMS,
-            |row| row.get::<_, Vec<u8>>(0),
-        )
-        .unwrap_or_default();
-
-    Ok(rlp::decode(&val).unwrap())
 }
 
 fn main() -> Result<(), JupiterError> {
